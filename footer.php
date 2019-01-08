5bf1cd5becaceb2fc06373786a90ebdfb6ecd43f
Galih Dea Pratama <galih.dpratama@gmail.com>
2019-01-08 10:52:32 +0700

    halaman admin belum lengkap
    dashboard admin (header sama footer halaman admin udah dipisah)

---
+++ footer.php
@@ -0,0 +1,32 @@
+<?php
+
+?>
+<div class="sidebar-menu">
+					<header class="logo">
+					<a href="#" class="sidebar-icon"> <span class="fa fa-bars"></span> </a> <a href="index.html"> <span id="logo"> <h1>UCCP</h1></span> 
+					<!--<img id="logo" src="" alt="Logo"/>--> 
+				  </a> 
+				</header>
+			<div style="border-top:1px solid rgba(69, 74, 84, 0.7)"></div>
+			<!--/down-->
+							<div class="down">	
+									  <a href="index.html"><img src="images/admin.jpg"></a>
+									  <a href="index.html"><span class=" name-caret">Jasmin Leo</span></a>
+									 <p>Karyawan Biasa</p>
+									<ul>
+																			<li><a class="tooltips" href="index.html"><span>Logout</span><i class="lnr lnr-power-switch"></i></a></li>
+										<li><a class="tooltips"><font color="black"><h5>Logout</></h5></a></li>
+										</ul>
+									</div>
+							   <!--//down-->
+                           <div class="menu">
+									<ul id="menu" >
+										<li><a href="karyawan.php"><i class="fa fa-tachometer"></i> <span>Dashboard</span></a></li>
+										 <li id="menu-academico" ><a href="#"><i class="fa fa-table"></i> <span> Absensi</span> <span class="fa fa-angle-right" style="float: right"></span></a>
+										   <ul id="menu-academico-sub" >
+											<li id="menu-academico-avaliacoes" ><a href="absenkaryawan.php"> Masuk</a></li>
+											<li id="menu-academico-boletim" ><a href="izin.php">Izin</a></li>
+											<li id="menu-academico-avaliacoes" ><a href="histori.php">Lihat History</a></li>
+											
+										  </ul>
+										</li>
