315f5e0154ec9bc550666b04ebb3d89ede83b8dd
Sayyid Yofa <sayyidyofa@gmail.com>
2019-01-09 07:52:45 +0700

    Merapikan kode
    Merapikan indentasi dan menghilangkan tag tanpa pasangan

--- menu_historikaryawan.php
+++ menu_historikaryawan.php
@@ -2,86 +2,79 @@
 <html>
 
 <body>
-		<?php include 'header.php'?>
-                   
-                    <!--/sub-heard-part-->
-                    <div class="sub-heard-part">
-                        <ol class="breadcrumb m-b-0">
-                            <li><a href="#">Home</a></li>
-                            <li class="active"> Riwayat Hadir</li>
-                        </ol>
-                    </div>
+    <?php include 'header.php'?>
 
-                <!--/tabs-inner-->
-                <!-- 	<div class="tab-inner">
-											      <div id="tabs" class="tabs">
-											 -->
-                <!-- masukkan kode di sini -->
+    <!--/sub-heard-part-->
+    <div class="sub-heard-part">
+        <ol class="breadcrumb m-b-0">
+            <li><a href="#">Home</a></li>
+            <li class="active"> Riwayat Hadir</li>
+        </ol>
+    </div>
 
+    <div id="tabs" class="tabs">
+        <h2 class="inner-tittle" style="text-align:center;">Histori Absen</h2>
+        <div class="graph">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>ID USER</th>
+                        <th>TIME</th>
+                        <th>STATUS</th>
+                        <th>LOKASI</th>
+                        <th>KETERANGAN</th>
+                        <th>ACC</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th>1</th>
+                        <th>001</th>
+                        <th>07:01:2019:11:02AM</th>
+                        <th>HADIR</th>
+                        <th>JL. Imam bonjol no 7</th>
+                        <th>Hadir</th>
+                        <th>YES</th>
+                    </tr>
+                    <tr>
+                        <th>2</th>
+                        <th>002</th>
+                        <th>07:01:2019:11:03AM</th>
+                        <th>SAKIT</th>
+                        <th>JL. umar bonjol no 1</th>
+                        <th>Tidak enak badan</th>
+                        <th>NO</th>
+                    </tr>
+                    <tr>
+                        <th>3</th>
+                        <th>003</th>
+                        <th>07:01:2019:00:03PM</th>
+                        <th>IZIN</th>
+                        <th>JL. teuke bonjol no 1</th>
+                        <th>IZIN PERGI</th>
+                        <th>YES</th>
+                    </tr>
+                </tbody>
+            </table>
+            <!-- /content -->
+        </div>
+        <!-- /tabs -->
 
-                <div id="tabs" class="tabs">
-                    <h2 class="inner-tittle" style="text-align:center;">Histori Absen</h2>
-                    <div class="graph">
-                        <table class="table table-striped">
-                            <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>ID USER</th>
-                                    <th>TIME</th>
-                                    <th>STATUS</th>
-                                    <th>LOKASI</th>
-                                    <th>KETERANGAN</th>
-                                    <th>ACC</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <th>1</th>
-                                    <th>001</th>
-                                    <th>07:01:2019:11:02AM</th>
-                                    <th>HADIR</th>
-                                    <th>JL. Imam bonjol no 7</th>
-                                    <th>Hadir</th>
-                                    <th>YES</th>
-                                </tr>
-                                <tr>
-                                    <th>2</th>
-                                    <th>002</th>
-                                    <th>07:01:2019:11:03AM</th>
-                                    <th>SAKIT</th>
-                                    <th>JL. umar bonjol no 1</th>
-                                    <th>Tidak enak badan</th>
-                                    <th>NO</th>
-                                </tr>
-                                <tr>
-                                    <th>3</th>
-                                    <th>003</th>
-                                    <th>07:01:2019:00:03PM</th>
-                                    <th>IZIN</th>
-                                    <th>JL. teuke bonjol no 1</th>
-                                    <th>IZIN PERGI</th>
-                                    <th>YES</th>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <!-- /content -->
-                    </div>
-                    <!-- /tabs -->
+    </div>
+    <nav align="center">
+        <ul class="pagination">
+            <li class="disabled"><a href="#" aria-label="Previous"><span aria-hidden="true">«</span></a></li>
+            <li class="active"><a href="#">1 <span class="sr-only">(current)</span></a></li>
+            <li><a href="#">2</a></li>
+            <li><a href="#">3</a></li>
+            <li><a href="#">4</a></li>
+            <li><a href="#">5</a></li>
+            <li><a href="#" aria-label="Next"><span aria-hidden="true">»</span></a></li>
+        </ul>
+    </nav>
 
-                </div>
-                <nav align="center">
-                    <ul class="pagination">
-                        <li class="disabled"><a href="#" aria-label="Previous"><span aria-hidden="true">«</span></a></li>
-                        <li class="active"><a href="#">1 <span class="sr-only">(current)</span></a></li>
-                        <li><a href="#">2</a></li>
-                        <li><a href="#">3</a></li>
-                        <li><a href="#">4</a></li>
-                        <li><a href="#">5</a></li>
-                        <li><a href="#" aria-label="Next"><span aria-hidden="true">»</span></a></li>
-                    </ul>
-                </nav>
-
-            <?php include 'footer.php';?>
+    <?php include 'footer.php';?>
 </body>
 
 </html>
