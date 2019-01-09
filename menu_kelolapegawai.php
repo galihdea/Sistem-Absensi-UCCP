f81b23089573163dde7f5d21bd883deafdf000ac
Galih Dea Pratama <galih.dpratama@gmail.com>
2019-01-09 11:41:26 +0700

    menu kelola
    rancangan tampilan menu kelola

--- menu_kelolapegawai.php
+++ menu_kelolapegawai.php
@@ -6,7 +6,34 @@
 		<h3 class="inner-tittle two">Kelola Pegawai </h3>
 		<div class="graph">
 			<div class="tables">
-				<table class="table table-hover"> <thead> <tr> <th>#</th> <th>First Name</th> <th>Last Name</th> <th>User Name</th> </tr> </thead> <tbody> <tr> <th scope="row">1</th> <td>Mark</td> <td>Otto</td> <td>@mdo</td> </tr> <tr> <th scope="row">2</th> <td>Jacob</td> <td>Thornton</td> <td>@fat</td> </tr> <tr> <th scope="row">3</th> <td>Larry</td> <td>the Bird</td> <td>@twitter</td> </tr> </tbody> </table>
+				<table class="table table-hover"> 
+					<thead> 
+						<tr> 
+							<th>#</th> 
+							<th>Username</th> 
+							<th>Nama Pegawai</th> 
+							<th>Aksi</th> 
+						</tr> 
+					</thead> 
+					<tbody> 
+						<?php
+							$i = 1;
+							$ambil_daftar_pegawai = $conn->query("SELECT * FROM pegawai");
+							while($pegawai=$ambil_daftar_pegawai->fetch_array()){
+								$id_luar = $pegawai['id_pegawai'];
+								$ambil_user_pegawai = $conn->query("SELECT * FROM user WHERE id_luar='$id_luar' AND status='Pegawai' ");
+								$user = $ambil_user_pegawai->fetch_array();
+								echo '<tr> 
+							<th scope="row">'.$i.'</th> 
+							<td>'.$user['username'].'</td> 
+							<td>'.$pegawai['nama_pegawai'].'</td>
+							<td>Lihat Ubah Hapus</td>  
+						</tr>';
+								$i++;
+							}
+						?>  
+					</tbody> 
+				</table>
 			</div>
 		</div>
  <?php include 'footer.php'; ?>
