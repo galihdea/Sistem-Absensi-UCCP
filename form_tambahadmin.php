da288e60ac0682c87194b92370bcc7a2630cc41d
Bintang H <stardemon45@gmail.com>
2019-01-14 12:18:48 +0700

    form_add

--- form_tambahadmin.php
+++ form_tambahadmin.php
@@ -5,36 +5,38 @@
 	<!--custom-widgets-->
 		<h3 class="inner-tittle two">Contoh Form Tambah Admin</h3>
 		<div class="grid-1">
-			<div class="form-body">
+			<div class="form-body margin-form">
 				<form method="POST" action="functions/tambah_admin.php" class="form-horizontal">
 					<div class="form-group">
-						<label for="focusedinput" class="col-sm-2 control-label">Username Admin</label>
-						<div class="col-sm-8">
+						<label for="focusedinput" class="col-sm-2 control-label ratakiri">Username Admin</label>
+						<div class="col-sm-10">
 							<input name="username" type="text" class="form-control1" id="focusedinput" placeholder="Username">
 						</div>
 					</div>
 					<div class="form-group"> 
-						<label for="focusedinput" class="col-sm-2 control-label">Password Admin</label>
-						<div class="col-sm-8">
+						<label for="focusedinput" class="col-sm-2 control-label ratakiri">Password Admin</label>
+						<div class="col-sm-10">
 							<input name="password" type="password" class="form-control" id="focusedinput" placeholder="Password"> 
 						</div>
 					</div>
 					<div class="form-group">
-						<label for="focusedinput" class="col-sm-2 control-label">Nama Admin</label>
-						<div class="col-sm-8">
+						<label for="focusedinput" class="col-sm-2 control-label ratakiri">Nama Admin</label>
+						<div class="col-sm-10">
 							<input name="namaadmin" type="text" class="form-control1" id="focusedinput" placeholder="Nama">
 						</div>
 					</div>
 					<div class="form-group">
-						<label for="selector1" class="col-sm-2 control-label">Status Admin</label>
-						<div class="col-sm-8">
+						<label for="selector1" class="col-sm-2 control-label ratakiri">Status Admin</label>
+						<div class="col-sm-10">
 							<select name="statusadmin" id="selector1" class="form-control1">
 								<option value="Super Admin">Super Admin</option>
 								<option value="Admin">Admin</option>
 							</select>
 						</div>
 					</div>
-					<button type="submit" class="btn btn-default">Submit</button>
+					<div  align="right">
+						<button type="submit" class="btn btn-default">Submit</button>
+					</div>
 				</form>
 			</div>
 		</div>
--- menu_kelolapegawai.php
+++ menu_kelolapegawai.php
@@ -8,38 +8,35 @@
     </script>
 	<!--custom-widgets-->
 		<h3 class="inner-tittle two">Kelola Pegawai </h3>
-		<div class="graph">
-			<div class="tables">
-				<table class="table table-hover"> 
-					<thead> 
-						<tr> 
-							<th>#</th> 
-							<th>Username</th> 
-							<th>Nama Pegawai</th> 
-							<th>Aksi</th> 
-						</tr> 
-					</thead> 
-					<tbody> 
-						<?php
-							$i = 1;
-							$ambil_daftar_pegawai = $conn->query("SELECT * FROM pegawai");
-							while($pegawai=$ambil_daftar_pegawai->fetch_array()){
-								$id_luar = $pegawai['id_pegawai'];
-								$ambil_user_pegawai = $conn->query("SELECT * FROM user WHERE id_luar='$id_luar' AND status='Pegawai' ");
-								$user = $ambil_user_pegawai->fetch_array();
-								echo '<tr> 
-							<th scope="row">'.$i.'</th> 
-							<td>'.$user['username'].'</td> 
-							<td>'.$pegawai['nama_pegawai'].'</td>
-							<td>Lihat Ubah Hapus</td>  
-						</tr>';
-								$i++;
-							}
-						?>  
-					</tbody> 
-				</table>
-			</div>
-		</div>
+			<div align="right"><a href="form_tambahpegawai.php" onclick="load_page()" class="btn blue four">Add</a></div>
+			<table id="example" class="table table-striped table-bordered" style="width:100%">
+				<thead> 
+					<tr> 
+						<th>#</th> 
+						<th>Username</th> 
+						<th>Nama Pegawai</th> 
+						<th>Aksi</th> 
+					</tr> 
+				</thead> 
+				<tbody> 
+					<?php
+						$i = 1;
+						$ambil_daftar_pegawai = $conn->query("SELECT * FROM pegawai");
+						while($pegawai=$ambil_daftar_pegawai->fetch_array()){
+							$id_luar = $pegawai['id_pegawai'];
+							$ambil_user_pegawai = $conn->query("SELECT * FROM user WHERE id_luar='$id_luar' AND status='Pegawai' ");
+							$user = $ambil_user_pegawai->fetch_array();
+							echo '<tr> 
+						<th scope="row">'.$i.'</th> 
+						<td>'.$user['username'].'</td> 
+						<td>'.$pegawai['nama_pegawai'].'</td>
+						<td><a class="btn blue four">Lihat</a> <a class="btn blue four">Ubah</a> <a class="btn blue four">Hapus</a></td>  
+					</tr>';
+							$i++;
+						}
+					?>  
+				</tbody> 
+			</table>
  <?php include 'footer.php'; ?>
 </body>
 </html>
