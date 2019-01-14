a187593f302faeaaa75f62278a94a9e2cd6012fa
Bintang H <stardemon45@gmail.com>
2019-01-14 09:51:37 +0700

    Menu_form

--- menu_form.php
+++ menu_form.php
@@ -3,34 +3,37 @@
 <body>
 	<?php include 'header.php'; ?>
 	<!--custom-widgets-->
-		<h3 class="inner-tittle two">Contoh Form </h3>
+		<h3 class="inner-tittle two">Contoh Form Tambah Admin</h3>
 		<div class="grid-1">
 			<div class="form-body">
 				<form class="form-horizontal">
 					<div class="form-group">
-						<label for="focusedinput" class="col-sm-2 control-label">Focused Input</label>
+						<label for="focusedinput" class="col-sm-2 control-label">Username</label>
 						<div class="col-sm-8">
 							<input type="text" class="form-control1" id="focusedinput" placeholder="Default Input">
 						</div>
-						<div class="col-sm-2">
-							<p class="help-block">Your help text!</p>
-						</div>
 					</div>
-					<div class="form-group">
-						<label for="disabledinput" class="col-sm-2 control-label">Disabled Input</label>
+					<div class="form-group"> 
+						<label for="focusedinput" class="col-sm-2 control-label">Password</label>
 						<div class="col-sm-8">
-							<input disabled="" type="text" class="form-control1" id="disabledinput" placeholder="Disabled Input">
+							<input type="password" class="form-control" id="focusedinput" placeholder="Password"> 
 						</div>
 					</div>
 					<div class="form-group">
-						<label for="selector1" class="col-sm-2 control-label">Dropdown Select</label>
+						<label for="focusedinput" class="col-sm-2 control-label">Nama</label>
+						<div class="col-sm-8">
+							<input type="text" class="form-control1" id="focusedinput" placeholder="Default Input">
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="selector1" class="col-sm-2 control-label">Status</label>
 						<div class="col-sm-8"><select name="selector1" id="selector1" class="form-control1">
-							<option>Lorem ipsum dolor sit amet.</option>
-							<option>Dolore, ab unde modi est!</option>
-							<option>Illum, fuga minus sit eaque.</option>
-							<option>Consequatur ducimus maiores voluptatum minima.</option>
+							<option>Super Admin</option>
+							<option>Admin</option>
+							<option>Pegawai</option>
 						</select></div>
 					</div>
+					<button type="submit" class="btn btn-default">Submit</button>
 				</form>
 			</div>
 		</div>
