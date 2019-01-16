106d0f86a65800c6c85f1d96348d67d6aa6ee222
Galih Dea Pratama <galih.dpratama@gmail.com>
2019-01-16 10:33:18 +0700

    form lihat
    udah bisa lihat admin sama pegawai

--- form_lihatadmin.php
+++ form_lihatadmin.php
@@ -1,7 +1,11 @@
 <!DOCTYPE HTML>
 <html>
 <body>
-	<?php include 'header.php'; ?>
+	<?php include 'header.php'; 
+		$id_admin = $_GET['id_admin'];
+		$querylihat = $conn->query("SELECT * FROM admin WHERE id_admin='$id_admin'");
+		$lihat = $querylihat->fetch_array();
+	?>
 	<script>
         //Value untuk mengisi Home / ....
         let page_header="Lihat Admin"
@@ -10,18 +14,18 @@
 		<h3 class="inner-tittle two">Contoh Form Lihat Admin</h3>
 		<div class="grid-1">
 			<div class="form-body margin-form">
-				<form method="POST" action="functions/tambah_admin.php" class="form-horizontal">
+				<form action="menu_kelolaadmin.php" class="form-horizontal">
 					<div class="form-group">
 						<label for="focusedinput" class="col-sm-2 control-label ratakiri">Username Admin</label>
-						<label class="col-sm-3 control-label lihat-data" id="user-admin">Username sampel</label>
+						<label class="col-sm-3 control-label lihat-data" id="user-admin"><?php echo $lihat['user_admin'];?></label>
 					</div>
 					<div class="form-group">
 						<label for="focusedinput" class="col-sm-2 control-label ratakiri">Nama Admin</label>
-						<label class="col-sm-3 control-label lihat-data" id="nama-admin">Nama sampel</label>
+						<label class="col-sm-3 control-label lihat-data" id="nama-admin"><?php echo $lihat['nama_admin'];?></label>
 					</div>
 					<div class="form-group">
 						<label for="selector1" class="col-sm-2 control-label ratakiri">Status Admin</label>
-						<label class="col-sm-3 control-label lihat-data" id="stat-admin">Status sampel</label>
+						<label class="col-sm-3 control-label lihat-data" id="stat-admin"><?php echo $lihat['status_admin'];?></label>
 					</div>
 					<div  align="right">
 						<button type="submit" class="btn btn-default">Kembali</button>
--- form_lihatpegawai.php
+++ form_lihatpegawai.php
@@ -1,7 +1,11 @@
 <!DOCTYPE HTML>
 <html>
 <body>
-	<?php include 'header.php'; ?>
+	<?php include 'header.php'; 
+		$id_pegawai = $_GET['id_pegawai'];
+		$querylihat = $conn->query("SELECT * FROM pegawai WHERE id_pegawai='$id_pegawai'");
+		$lihat = $querylihat->fetch_array();
+	?>
 	<script>
         //Value untuk mengisi Home / ....
         let page_header="Lihat Pegawai"
@@ -10,22 +14,22 @@
 		<h3 class="inner-tittle two">Contoh Form Lihat Pegawai</h3>
 		<div class="grid-1">
 			<div class="form-body">
-				<form method="POST" action="functions/tambah_pegawai.php" class="form-horizontal">
+				<form action="menu_kelolapegawai.php" class="form-horizontal">
 					<div class="form-group">
 						<label for="focusedinput" class="col-sm-2 control-label lihat-data">Username Pegawai</label>
-						<label class="col-sm-3 control-label lihat-data" id="user-pegawai">Username sampel</label>
+						<label class="col-sm-3 control-label lihat-data" id="user-pegawai"><?php echo $lihat['user_pegawai'];?></label>
 					</div>
 					<div class="form-group">
 						<label for="focusedinput" class="col-sm-2 control-label lihat-data">Nama Pegawai</label>
-						<label class="col-sm-3 control-label lihat-data" id="nama-pegawai">Nama sampel</label>
+						<label class="col-sm-3 control-label lihat-data" id="nama-pegawai"><?php echo $lihat['nama_pegawai'];?></label>
 					</div>
 					<div class="form-group">
 						<label for="selector1" class="col-sm-2 control-label lihat-data">Divisi Pegawai</label>
-						<label class="col-sm-3 control-label lihat-data" id="div-pegawai">Divisi sampel</label>
+						<label class="col-sm-3 control-label lihat-data" id="div-pegawai"><?php echo $lihat['divisi_pegawai'];?></label>
 					</div>
 					<div class="form-group">
 						<label for="selector1" class="col-sm-2 control-label lihat-data">Status Pegawai</label>
-						<label class="col-sm-3 control-label lihat-data" id="stat-pegawai">Status sampel</label>
+						<label class="col-sm-3 control-label lihat-data" id="stat-pegawai"><?php echo $lihat['status_pegawai'];?></label>
 					</div>
 					<button type="submit" class="btn btn-default">Kembali</button>
 				</form>
