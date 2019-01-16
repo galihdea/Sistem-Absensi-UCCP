0624c0f83a16b6731bc6852fc5bfd6a0b85757c7
Sayyid Yofa <sayyidyofa@gmail.com>
2019-01-16 10:14:09 +0700

    CSS Tambahan

--- form_tambahpegawai.php
+++ form_tambahpegawai.php
@@ -10,25 +10,25 @@
 					<div class="form-group">
 						<label for="focusedinput" class="col-sm-2 control-label">Username Pegawai</label>
 						<div class="col-sm-8">
-							<input name="username" type="text" class="form-control1" id="focusedinput" placeholder="Username">
+							<input name="username" type="text" class="form-control1" id="focusedinput" placeholder="Username" required>
 						</div>
 					</div>
 					<div class="form-group"> 
 						<label for="focusedinput" class="col-sm-2 control-label">Password Pegawai</label>
 						<div class="col-sm-8">
-							<input name="password" type="password" class="form-control" id="focusedinput" placeholder="Password"> 
+							<input name="password" type="password" class="form-control" id="focusedinput" placeholder="Password" required> 
 						</div>
 					</div>
 					<div class="form-group">
 						<label for="focusedinput" class="col-sm-2 control-label">Nama Pegawai</label>
 						<div class="col-sm-8">
-							<input name="namapegawai" type="text" class="form-control1" id="focusedinput" placeholder="Nama">
+							<input name="namapegawai" type="text" class="form-control1" id="focusedinput" placeholder="Nama" required>
 						</div>
 					</div>
 					<div class="form-group">
 						<label for="selector1" class="col-sm-2 control-label">Divisi Pegawai</label>
 						<div class="col-sm-8">
-							<select name="divisipegawai" id="selector1" class="form-control1">
+							<select name="divisipegawai" id="selector1" class="form-control1" required>
 								<option value="A">A</option>
 								<option value="B">B</option>
 								<option value="C">C</option>
@@ -38,7 +38,7 @@
 					<div class="form-group">
 						<label for="selector1" class="col-sm-2 control-label">Status Pegawai</label>
 						<div class="col-sm-8">
-							<select name="statuspegawai" id="selector1" class="form-control1">
+							<select name="statuspegawai" id="selector1" class="form-control1" required>
 								<option value="Manager Divisi">Manager Divisi</option>
 								<option value="Staf Divisi">Staf Divisi</option>
 								<option value="Intern">Intern</option>
