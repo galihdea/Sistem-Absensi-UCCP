7ab2d7b5470d5fe078e09e468fc2d6826e0c0f54
Galih Dea Pratama <galih.dpratama@gmail.com>
2019-01-17 10:34:46 +0700

    fungsi buat absen
    masuk ke database

--- functions/absen_izin.php
+++ functions/absen_izin.php
@@ -1,16 +1,24 @@
 <?php
 	session_start();
 	include 'koneksi.php';
-	$id_pegawai = $_SESSION['id_pegawai'];
+	$id_user = $_SESSION['id_user'];
+
 	$izin = $_POST['izin'];
 	if($izin=='sakit'){
 		$keterangan = $_POST['keterangan_sakit'];
+		$acc = "Approved";
 	}
 	elseif($izin=='izin'){
 		$keterangan = $_POST['keterangan_izin'];
+		$acc = "Pending";
 	}
 	elseif($izin=='cuti'){
 		$keterangan = $_POST['keterangan_cuti_awal']. " - ".$_POST['keterangan_cuti_akhir'];
+		$acc = "Pending";
 	}
-	header('location:../menu_dashboard.php');
+	$queryizin = $conn->query("INSERT INTO absensi (id_user,status_absensi,status_acc,keterangan) VALUES ('$id_user','$izin','$acc','$keterangan')");
+
+	if($queryizin){
+		header('location:../menu_dashboard.php');
+	}
 ?>
