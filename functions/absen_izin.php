af82dbe222cdee944c74db7df93f5bdbcb5d748c
Galih Dea Pratama <galih.dpratama@gmail.com>
2019-01-17 11:35:44 +0700

    revisi fungsi buat absen masuk sama izin
    udah bisa ndetek ip address

--- functions/absen_izin.php
+++ functions/absen_izin.php
@@ -2,6 +2,7 @@
 	session_start();
 	include 'koneksi.php';
 	$id_user = $_SESSION['id_user'];
+	$ip_address = $_SERVER["REMOTE_ADDR"];
 
 	$izin = $_POST['izin'];
 	if($izin=='sakit'){
@@ -16,7 +17,7 @@
 		$keterangan = $_POST['keterangan_cuti_awal']. " - ".$_POST['keterangan_cuti_akhir'];
 		$acc = "Pending";
 	}
-	$queryizin = $conn->query("INSERT INTO absensi (id_user,status_absensi,status_acc,keterangan) VALUES ('$id_user','$izin','$acc','$keterangan')");
+	$queryizin = $conn->query("INSERT INTO absensi (id_user,status_absensi,status_acc,keterangan,ip_address) VALUES ('$id_user','$izin','$acc','$keterangan','$ip_address')");
 
 	if($queryizin){
 		header('location:../menu_dashboard.php');
--- functions/absen_masuk.php
+++ functions/absen_masuk.php
@@ -1,6 +1,7 @@
 <?php
 	session_start();
 	include 'koneksi.php';
+	$ip_address = $_SERVER["REMOTE_ADDR"];
 	$id_user = $_SESSION['id_user'];
 	$id_pegawai = $_SESSION['id_pegawai'];
 	$querypegawai = $conn->query("SELECT * FROM user WHERE status='Pegawai' AND id_luar='$id_pegawai'");
@@ -10,7 +11,7 @@
 	if($pegawai['username']==$user_form&&$pegawai['password']==$pass_form){
 		$status = "masuk";
 		$acc = "Approved";
-		$querymasuk = $conn->query("INSERT INTO absensi (id_user,status_absensi,status_acc) VALUES ('$id_user','$status','$acc')");
+		$querymasuk = $conn->query("INSERT INTO absensi (id_user,status_absensi,status_acc,ip_address) VALUES ('$id_user','$status','$acc','$ip_address')");
 		if($querymasuk){
 			header('location:../menu_dashboard.php');
 		}
