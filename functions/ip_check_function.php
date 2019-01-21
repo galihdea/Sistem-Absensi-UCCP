5115bb2f8e474c621fceab80eb9ae28a30ecb693
Galih Dea Pratama <galih.dpratama@gmail.com>
2019-01-21 09:57:24 +0700

    update fungsi cek ip

--- functions/ip_check_function.php
+++ functions/ip_check_function.php
@@ -2,7 +2,7 @@
 	
 	function ambilIP($ip_address){
 		$alamat_ambil = "http://api.ipstack.com/".$ip_address."?access_key=69e537e14e9cc6ebdfe23c03cd700118";
-		$ambil = json_decode($alamat_ambil);
+		$ambil = json_decode(file_get_contents($alamat_ambil));
 		return $ambil;
 	}
 	
