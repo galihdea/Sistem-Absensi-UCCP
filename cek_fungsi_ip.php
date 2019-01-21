084f8aeecc908a9005e07242b419aabf2317ec7a
Galih Dea Pratama <galih.dpratama@gmail.com>
2019-01-21 10:25:02 +0700

    ada ada aja part 2

--- cek_fungsi_ip.php
+++ cek_fungsi_ip.php
@@ -3,7 +3,7 @@
 	include 'functions/ip_check_function.php';
 
     $ip_address=$_SERVER['REMOTE_ADDR'];
-    //$ip_address=$_SERVER['HTTP_X_FORWARDED_FOR'];
+    $ip_address=$_SERVER['HTTP_X_FORWARDED_FOR'];
     //$ip_address=$_SERVER['HTTP_CLIENT_IP'];
 
     $ambilLah = ambilIP($ip_address);
