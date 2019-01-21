21ef7493e528ede976c7960754388de0d80bc65c
Galih Dea Pratama <galih.dpratama@gmail.com>
2019-01-21 10:23:57 +0700

    ada ada saja

--- cek_fungsi_ip.php
+++ cek_fungsi_ip.php
@@ -2,18 +2,9 @@
 	include 'functions/koneksi.php';
 	include 'functions/ip_check_function.php';
 
-	if (!empty($_SERVER['HTTP_CLIENT_IP']))   //check ip from share internet
-    {
-      $ip_address=$_SERVER['HTTP_CLIENT_IP'];
-    }
-    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))   //to check ip is pass from proxy
-    {
-      $ip_address=$_SERVER['HTTP_X_FORWARDED_FOR'];
-    }
-    else
-    {
-      $ip_address=$_SERVER['REMOTE_ADDR'];
-    }
+    $ip_address=$_SERVER['REMOTE_ADDR'];
+    //$ip_address=$_SERVER['HTTP_X_FORWARDED_FOR'];
+    //$ip_address=$_SERVER['HTTP_CLIENT_IP'];
 
     $ambilLah = ambilIP($ip_address);
 
