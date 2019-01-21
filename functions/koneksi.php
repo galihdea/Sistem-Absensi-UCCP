3daf72dbb4dd17e264759a84579ace2e83fd4e0f
Sayyid Yofa <sayyidyofa@gmail.com>
2019-01-21 10:00:09 +0700

    Auto stash before merge of "master" and "origin/master"

--- functions/koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$db_host = 'localhost';
-  	$db_user = 'root';
-  	$db_pass = '4f13541c6c5fa6c1ad0dd9d60c60d470ad165941cec2bd5f';
-  	$db_name = 'sauccp';
-  	$conn =  new mysqli($db_host,$db_user,$db_pass,$db_name) or die();
-?>
