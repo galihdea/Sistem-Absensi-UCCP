bf1c631e44039543118f1211045ac78f05dedd24
anonymousliem <muslimsyuhadah@gmail.com>
2019-01-22 10:46:16 +0700

    menu dashbord
    menyamakan bentuk dan ukuran

--- menu_dashboard.php
+++ menu_dashboard.php
@@ -15,6 +15,8 @@
     </script>
 	<!--Dashboard Admin and Super Admin-->
 	<?php if ($_SESSION['jenis_user'] == "Super Admin" || $_SESSION['jenis_user'] == "Admin"){?>
+	
+	<!--kolom JUMLAH PEGAWAI -->
 	<div class="col-sm-12">
 		<div class="col-sm-2"></div>
 		<a href="menu_kelolapegawai.php" onclick="load_page()">
@@ -36,7 +38,54 @@
 		</a>
 		<div class="col-sm-2"></div>
 	</div>
+<!--kolom Hari ini PEGAWAI -->
 	<div class="col-sm-12">
+		<div class="col-sm-2"></div>
+		<a href="menu_lihatlog.php" onclick="load_page()">
+		<div class="col-sm-8 bggreen2 dashboard" align="center" style="margin-bottom: 10px;">
+			<div class="col-sm-3">
+				<i class="fa fa-check-circle" style="margin: 20px 0px !important; font-size:70px; margin:10px; color:white;"></i>
+			</div>
+			<div class="col-sm-6">
+				<p style="font-size:20px; margin:10px 0px 0px 5px; color:white; text-align:left;">Hari Ini</p>
+				<p style="font-size:40px; margin:5px; color:white; text-align:left;">Masuk</p>
+			</div>
+			<div class="col-sm-3">
+				<p style="font-size:60px; margin:12px; color:white;"><?php echo $jumlahmasuk;?></p>
+			</div>
+			<div class="col-sm-12">
+				<p class="dashboard-detail">Lihat Daftar Absen..</p>
+			</div>
+		</div>
+		</a>
+		<div class="col-sm-2"></div>
+	</div>
+	
+<!--kolom HARI INI ABSEN -->	
+	<div class="col-sm-12">
+		<div class="col-sm-2"></div>
+		<a href="menu_cuti.php" onclick="load_page()">
+		<div class="col-sm-8 bgred2 dashboard" align="center" style="margin-bottom: 10px;">
+			<div class="col-sm-3">
+				<i class="fa fa-minus-circle" style="margin: 20px 0px !important; font-size:70px; margin:10px; color:white;"></i>
+			</div>
+			<div class="col-sm-6">
+				<p style="font-size:20px; margin:10px 0px 0px 5px; color:white; text-align:left;">Hari Ini</p>
+				<p style="font-size:40px; margin:5px; color:white; text-align:left;">Absen</p>
+			</div>
+			<div class="col-sm-3">
+				<p style="font-size:60px; margin:12px; color:white;"><?php echo $jumlahtidakmasuk;?></p>
+			</div>
+			<div class="col-sm-12">
+				<p class="dashboard-detail">Lihat Pengajuan Absen</p>
+			</div>
+		</div>
+		</a>
+		<div class="col-sm-2"></div>
+	</div>
+
+
+	<!-- <div class="col-sm-12">
 		<div class="col-sm-2"></div>
 		<a href="menu_lihatlog.php" onclick="load_page()">
 		<div class="col-sm-4 bggreen2 dashboard" align="center" style="width: 32.83%;">
@@ -74,7 +123,7 @@
 		</div>
 		</a>
 		<div class="col-sm-2"></div>
-	</div>
+	</div> -->
 	<!--Dashboard Pegawai Belum Absen-->	
 	<?php } if ($_SESSION['jenis_user'] == "Pegawai" && $boleh_absen == "Boleh"){?>
 	<div class="col-sm-12">
