d860811644edde01c9e31c5dbdf0f77b7a020ce0
Galih Dea Pratama <galih.dpratama@gmail.com>
2019-01-28 10:21:46 +0700

    reset password
    udah bisa reset password insyaAllah

--- form_resetpassword.php
+++ form_resetpassword.php
@@ -7,38 +7,60 @@
         let page_header="Reset Password"
     </script>
 	<!--custom-widgets-->
+	<?php
+		$id_user = $_GET['id_user'];
+		$cekuser = $conn->query("SELECT * FROM user WHERE id_user='$id_user'");
+		$user = $cekuser->fetch_array();
+		$usernama = $user['username'];
+		$id_luar = $user['id_luar'];
+		if($user['status']=='Pegawai'){
+			$ambildata = $conn->query("SELECT * FROM pegawai WHERE id_pegawai='$id_luar'");
+			$ambil = $ambildata->fetch_array();
+			$nama = $ambil['nama_pegawai'];
+		}
+		elseif($user['status']=='Admin'){
+			$ambildata = $conn->query("SELECT * FROM admin WHERE id_admin='$id_luar'");
+			$ambil = $ambildata->fetch_array();
+			$nama = $ambil['nama_admin'];
+		}
+		elseif($user['status']=='Super Admin'){
+			$ambildata = $conn->query("SELECT * FROM super_admin WHERE id_sadmin='$id_luar'");
+			$ambil = $ambildata->fetch_array();
+			$nama = $ambil['nama_sadmin'];
+		}
+	?>
 		<h3 class="inner-tittle two">Reset Password</h3>
 		<div class="grid-1">
 			<div class="form-body">
-				<form method="POST" action="functions/reset_password.php" class="form-horizontal">
+				<form method="POST" action="functions/reset_password.php?id_user=<?php echo $id_user;?>" class="form-horizontal">
 					<div class="form-group">
 						<label for="focusedinput" class="col-sm-2 control-label">Username</label>
 						<div class="col-sm-8">
-							<input name="username" type="text" class="form-control1" id="focusedinput" placeholder="Username" disabled>
+							<input name="username" type="text" class="form-control1" id="focusedinput" placeholder="Username" value="<?php echo $usernama;?>" disabled>
 						</div>
 					</div>
 					<div class="form-group">
 						<label for="focusedinput" class="col-sm-2 control-label">Nama</label>
 						<div class="col-sm-8">
-							<input name="namapegawai" type="text" class="form-control1" id="focusedinput" placeholder="Nama" disabled>
+							<input name="namapegawai" type="text" class="form-control1" id="focusedinput" placeholder="Nama" value="<?php echo $nama;?>" disabled>
 						</div>
 					</div>
 					<div class="form-group"> 
 						<label for="focusedinput" class="col-sm-2 control-label">Password Lama</label>
 						<div class="col-sm-8">
-							<input name="password" type="password" class="form-control" id="focusedinput" placeholder="Password Lama" required> 
+							<input name="passwordlama" type="password" class="form-control" id="focusedinput" placeholder="Password Lama" required> 
 						</div>
 					</div>
 					<div class="form-group"> 
 						<label for="focusedinput" class="col-sm-2 control-label">Password Baru</label>
 						<div class="col-sm-8">
-							<input name="password" type="password" class="form-control" id="focusedinput" placeholder="Retype Password Baru" required> 
+							<input name="passwordbaru" type="password" class="form-control" id="focusedinput" placeholder="Password Baru" required> 
 						</div>
 					</div>
 					<div class="form-group"> 
 						<label for="focusedinput" class="col-sm-2 control-label">Retype Password</label>
 						<div class="col-sm-8">
-							<input name="password" type="password" class="form-control" id="focusedinput" placeholder="Retype Password" required> 
+							<input name="passwordbaruconfirm" type="password" class="form-control" id="focusedinput" placeholder="Retype Password Baru" required> 
 						</div>
 					</div>
                     <a href="menu_dashboard.php" class="btn blue four bgred tombol-back">Cancel</a>
---
+++ functions/reset_password.php
@@ -0,0 +1,28 @@
+<?php
+	include 'koneksi.php';
+	$id_user = $_GET['id_user'];
+	$passlama = md5($_POST['passwordlama']);
+	$cekpass = $conn->query("SELECT * FROM user WHERE id_user='$id_user' AND password='$passlama'");
+	$bener = mysqli_num_rows($cekpass);
+	$lokasi = '';
+	if($bener==0){
+		$lokasi = 'location:../form_resetpassword.php?id_user='.$id_user;
+	}
+	elseif($bener==1){
+		$passbaru = md5($_POST['passwordbaru']);
+		$passconfirm = md5($_POST['passwordbaruconfirm']);
+		if($passbaru==$passbaruconfirm){
+			$ubahpass = $conn->query("UPDATE user SET password='$passbaru' WHERE id_user='$id_user'");
+			if($ubahpass){
+				$lokasi = 'location:../menu_dashboard.php';
+			}
+			else{
+				$lokasi = 'location:../form_resetpassword.php?id_user='.$id_user;
+			}
+		}
+		else{
+			$lokasi = 'location:../form_resetpassword.php?id_user='.$id_user;
+		}
+	}
+	header($lokasi);
+?>
