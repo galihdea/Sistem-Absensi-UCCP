cfdef186ba514aeb07ea31bf39e69be2ef2fda2c
Galih Dea Pratama <galih.dpratama@gmail.com>
2019-02-06 11:37:34 +0700

    revisi fungsi ubah foto

--- functions/ubah_foto.php
+++ functions/ubah_foto.php
@@ -1,14 +1,6 @@
 <?php
 	include 'koneksi.php';
 
-	function simpanGambar($id_user,$image){
-		$query_simpan = $conn->query("UPDATE user SET gambar='$image' WHERE id_user='$id_user'");
-		if($query_simpan){
-			$loco = 'location:../form_profile.php?id_user='.$id_user.'';
-			header($loco);
-		}
-	}
-
 	$id_user = $_GET['id_user'];
 
 	if(getimagesize($_FILES['gambar']['tmp_name'])==FALSE){
@@ -17,6 +9,10 @@
 	else{
 		$image = $_FILES['gambar']['tmp_name'];
 		$image = addslashes(file_get_contents($image));
-		simpanGambar($id_user,$image);
+		$query_simpan = $conn->query("UPDATE user SET gambar='$image' WHERE id_user='$id_user'");
+		if($query_simpan){
+			$loco = 'location:../form_profile.php?id_user='.$id_user.'';
+			header($loco);
+		}
 	}
 ?>
