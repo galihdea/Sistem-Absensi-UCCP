ba728861c4de5ca47b763dc67c4caa27248d8eea
Sayyid Yofa <sayyidyofa@gmail.com>
2019-02-13 15:04:17 +0700

    fix izin dan sakit
    kurang upload ket. sakit

--- menu_absenizin.php
+++ menu_absenizin.php
@@ -32,8 +32,7 @@
                                         Izin</label>
                                     <div class="col-sm-8">
                                         <select name="izin" id="selector1" class="form-control1" onchange="pilihForm(this.value)">
-                                            <option value="" disabled selected style="">---Pilih
-                                                Jenis Izin---</option>
+                                            <option value="" disabled selected style="">---Pilih Jenis Izin---</option>
                                             <option value="sakit">SAKIT</option>
                                             <option value="izin">IZIN</option>
                                         </select></div>
@@ -45,18 +44,20 @@
 										let form_izin = document.getElementById("form_izin");
 										switch(form_param){
 										case "sakit":
-										    form_izin.innerHTML='';form_cuti.innerHTML='';
+										    form_izin.innerHTML='';form_sakit.innerHTML='';
 											form_sakit.innerHTML='<div class="form-group"><label for="txtarea1" class="col-sm-2 control-label" style="text-align:left;">Keterangan Sakit</label><div class="col-sm-8"><textarea name="keterangan_sakit" id="txtarea1" cols="50" rows="4" class="form-control1" required></textarea></div></div><div class="form-group"><button type="submit" class="btn blue four bglblue hover">Ajukan Izin</button></div>';
 											break;
 										case "izin":
-											form_sakit.innerHTML='';form_cuti.innerHTML='';
+											form_sakit.innerHTML='';form_izin.innerHTML='';
 											form_izin.innerHTML='<div class="form-group"><label for="txtarea1" class="col-sm-2 control-label" style="text-align:left;">Keterangan Izin</label><div class="col-sm-8"><textarea name="keterangan_izin" id="txtarea1" cols="50" rows="4" class="form-control1" required></textarea></div></div><div class="form-group"><button type="submit" class="btn blue four bglblue hover">Ajukan Izin</button></div>';
 											break;
 											}
 										}
 								</script>
+
                                 <div id="form_sakit"></div>
                                 <div id="form_izin"></div>
+
                             <div class="clearfix"></div>
                             </form>
                             
